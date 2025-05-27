18ff607079dc892c0978c23f30b202753d0c446c
Tharushi Sooriyabandara <sooriyabandaratharushi@gmail.com>
2025-05-27 18:08:35 +0530

    Update rampupdown.js
    { duration: '10s', target: 5 }, // Ramp up to 5 users over 10 seconds
    
    { duration: '20s', target: 3 }  // Ramp down to 3 users over 20 seconds

--- Tests/rampupdown.js
+++ Tests/rampupdown.js
@@ -1,14 +1,14 @@
-import http from 'k6/http'
+import http from 'k6/http';
 
-// rampup nd ramp down users
+// Ramp-up and Ramp-down users
 export let options = {
-    stages: {
-        {duration: '10s', target: 5}, // 5 users for 10 seconds
-        {duration}   
-    }
-}
+    stages: [
+        { duration: '10s', target: 5 }, // Ramp up to 5 users over 10 seconds
+        { duration: '20s', target: 3 }  // Ramp down to 3 users over 20 seconds
+    ]
+};
 
-// main function, VU will call endpoint
+// Main function, VUs will call this endpoint repeatedly
 export default function () {
-    http.get('https://www.google.com')
+    http.get('https://www.google.com');
 }
