ae45e0021bd4fdac0cd946197b2812d29ce3fd77
Tharushi Sooriyabandara <sooriyabandaratharushi@gmail.com>
2025-05-27 17:01:16 +0530

    Update script.js

--- Tests/script.js
+++ Tests/script.js
@@ -7,8 +7,15 @@ import http from 'k6/http'
 
 // main function where user will be spread
 export default function () {
+    // VU code, gets called again and again bu VU
     http.get('https://www.google.com/')
     
 }
 
 // lets us execute test with 1 virtual user
+
+// command not found - becuae env avriable not set, so restart visual studio or machine
+
+// press tab to auto fill values
+
+// test successfully executed
