65eab73a99c9595a9c9ba7577b2c0a4f1e7e0850
Tharushi Sooriyabandara <sooriyabandaratharushi@gmail.com>
2025-05-28 12:06:49 +0530

    multiple checks

--- Tests/check.js
+++ Tests/check.js
@@ -1,28 +1,15 @@
-/*
-
-Lets us create mock API for tetsing - Dummy API -
-https://designer.mocky.io/ - use this
-
-
-now, do performance test on https://run.mocky.io/v3/32acd9e1-c313-41ea-b4f6-5474eff95fb3
-
-*/
-
 import http from 'k6/http';
 import { check } from 'k6';
 
 export default function () {
-    // Send GET request to the mock API
     let response = http.get('https://run.mocky.io/v3/fa54f59e-55bb-42d4-b0f4-53bf03b7ffd0');
 
-    // Log the response body length, current Virtual User, and Iteration
-    console.log(`response body length ${response.body.length} for VU= ${__VU} ITER = ${__ITER}`);
+    console.log(`Response body length: ${response.body.length}`);
 
-    // Perform checks (assertions that don't stop the test)
     check(response, {
-        'is response status 200:'  : (r) => r.status === 200,
-        'body size is 43 bytes:'   : (r) => r.body.length === 44,
+        'status is 200'                   : (r) => r.status === 200,
+        'body is not empty'               : (r) => r.body.length > 0,
+        //'content includes "message"'      : (r) => r.body.includes('message'),
+        [`body size is ${response.body.length}`] : (r) => r.body.length === response.body.length,
     });
 }
-
-
