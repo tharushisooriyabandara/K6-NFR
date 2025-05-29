14befce7864c630fd4f36669cf9cec04e10bdab4
Tharushi Sooriyabandara <sooriyabandaratharushi@gmail.com>
2025-05-29 10:47:19 +0530

    Update http-get-simple-json.js

--- Tests/http-get-simple-json.js
+++ Tests/http-get-simple-json.js
@@ -8,24 +8,20 @@ export default function () {
         headers: {
             'Content-Type': 'application/json',
         },
-    }
+    };
 
     const response = http.get(url, headerParam);
 
-    // Check for HTTP 200
     check(response, {
         'Status is 200': (r) => r.status === 200,
-    })
+    });
 
-    // Parse response
     let body = JSON.parse(response.body);
 
-    // Log outputs
     console.log(`Response body: ${JSON.stringify(body)}`);
-    console.log(`Message: ${body.Message}`); // Update key if different
+    console.log(`Message: ${body.Message}`);
 
-    // Check for expected message
     check(response, {
-        'Is message success: ': (r) => JSON.parse(r.body).Message === "Data fetched successfully",
+        'Is message success: ': (r) => JSON.parse(r.body).Message === "Data Fetched Successfully",
     });
 }
