6f0d4e32a39805cd18e75d18ee6079964499463c
Tharushi Sooriyabandara <sooriyabandaratharushi@gmail.com>
2025-06-06 11:31:31 +0530

    Create env.js

---
+++ env.js
@@ -0,0 +1,16 @@
+/*
+
+env.js – Centralized Environment Config
+
+This file defines environment-specific URLs and settings.
+
+Example for testing google.com:
+
+dev: http://www.google-dev.com – used by developers for continuous deployment
+
+int: http://www.google-int.com – used by testers for integration and regression testing
+
+prod: http://www.google.com – live production environment
+
+*/
+
