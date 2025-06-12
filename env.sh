96d8f43d192fc13cf991d00092b089a091ae6624
Tharushi Sooriyabandara <sooriyabandaratharushi@gmail.com>
2025-06-12 09:26:46 +0530

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,25 +1,11 @@
-# ----------------------------------------
-# Framework Overview
-#
-# We are designing a Test Automation Framework for Moodle.
-# The first step is to define the environment setup.
-# ----------------------------------------
+# Local Environment Configuration
+# This file is NOT committed to Git (contains sensitive/runtime data)
 
-# ----------------------------------------
-# Environment Setup
-# ----------------------------------------
-
-# 1. env.sh – Local Environment Configuration (Shell File)
-#
-# - This file contains sensitive and runtime-configurable data.
-# - It will not be committed to Git. Each team member will have their own local copy.
-# - It includes:
-#   - Sensitive data such as usernames, passwords, API tokens, and secret keys
-#   - Runtime values such as the number of virtual users for tools like k6
-#     Example: Instead of hardcoding `k6 run --vus 10`, use `k6 run --vus $VIRTUAL_USERS` from env.sh
-
-
-export MOODLE_ADMIN = "admin"
-export MOODLE_ADMIN_PASSWORD = "Admin123!@#"
-export VU = 5
-export DURATION = "10s"
+export MOODLE_ADMIN="admin"
+export MOODLE_ADMIN_PASSWORD="Admin123!@#"
+export VUS=1
+export DURATION="5s"
+export INT_TOKEN="585a5e34abe199537fec2640b8252ef7"
+export DEV_TOKEN="adddummytokenhere"
+export PROD_TOKEN="adddummytokenhere"
+export ENVIRONMENT="int"
